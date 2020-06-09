adaf35e46ec922bf95f1c42a559765c959a24101
T-Gamer <marquesde2013@gmail.com>
2020-06-09 14:50:07 -0300

    itens controler server config

--- ecoleta-server/src/controllers/ItemsController.js
+++ ecoleta-server/src/controllers/ItemsController.js
@@ -9,11 +9,11 @@ module.exports={
               return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.0.7:3333/uploads/${item.image}`,
+                image_url: `http://127.0.0.1:3333/uploads/${item.image}`,
               };
             });
         
-            return response.json(serializedItems);
+            return Response.json(serializedItems);
         }catch(error){
             next(error)
         }

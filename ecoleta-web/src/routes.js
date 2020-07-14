901ed455bab72bfa99cbaa9c572c5a10a5524cd8
T-Gamer <marquesde2013@gmail.com>
2020-07-13 22:49:46 -0300

    Configuração da Router and implementação home

--- ecoleta-web/src/routes.js
+++ ecoleta-web/src/routes.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 
-const Routes = ()=>{
+const Routes = () =>{
     return (
-        <BrowserRouter></BrowserRouter>
+        <BrowserRouter>
+            <Switch>
+                 <Route component={Home} exact path="/" />
+            </Switch>           
+        </BrowserRouter>
     );
-}
+};
 export default Routes;
